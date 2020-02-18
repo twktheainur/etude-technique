a4486597f959bbcc943417787586c285b0b80dd7
agricoptere <supergop34@gmail.com>
2020-02-18 16:22:56 +0100

    kzejf

--- script.py
+++ script.py
@@ -5,21 +5,29 @@ import re
 
 def get_body_text(root):
     for child in root.getchildren():
-        print(child.tag)
         if 'text' in child.tag:
             for subchild in child.getchildren():
-                print(subchild.tag)
                 if 'body' in subchild.tag:
                     return ' '.join(subchild.itertext())
 
 file = pd.read_excel('https://github.com/Kipre/etude-technique/blob/master/2020_export_Projet_Indexation_Automatique_Notice_accesTI_public_depuis2010_20200204.xlsx?raw=true')
+file = file.loc[file.LANGUE_DOC=="fre"]
 
 for i, pdf in file['ACCES_TEXTE_INTEGRAL'].iteritems():
     r = requests.get(pdf)
-    r = requests.post('http://cloud.science-miner.com/grobid/api/processFulltextDocument', files={'input': r.content})
+    r = requests.post('http://cloud.science-miner.com/grobid/api/processFulltextDocument', 
+    	              files={'input': r.content})
     xml = r.text
     pattern = re.compile(r'<\?xml.*\?>')
     xml = pattern.sub('', xml)
     root = etree.fromstring(xml)
     fulltext = get_body_text(root)
-    
+    alphanumeric = re.compile("([^\s\w']|_|\n|\t)+")
+    fulltext = alphanumeric.sub(' ', fulltext)
+    query = '{"text": "' + fulltext + '", "language": {"lang": "fr"} }'
+    r = requests.post('http://localhost:8090/service/disambiguate', 
+    	              files={'query': query})
+    print(r.text)
+
+    if i == 0:
+    	break
