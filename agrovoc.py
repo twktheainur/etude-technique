36428082c0a4b60a829205888901606924f673e9
agricoptere <supergop34@gmail.com>
2020-02-21 10:19:18 +0100

    moved to notebook

--- agrovoc.py
+++ agrovoc.py
@@ -19,45 +19,41 @@ class Agrovoc:
 
         graph.parse('data/agrovoc.rdf')
 
+        string_entries = []
+        pref_labels = graph.query("""SELECT ?x ?lf WHERE {
+            ?x a skos:Concept;
+            skosxl:prefLabel ?l.
+            ?l skosxl:literalForm ?lf.
+        }
+        """, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#"),
+                         skosxl=Namespace("http://www.w3.org/2008/05/skos-xl#")))
 
+        for result in pref_labels:
+            string_entries.append((str(result[0]), str(result[1])))
 
-g = Graph()
-a = Agrovoc(g)
-    #     string_entries = []
-    #     pref_labels = graph.query("""SELECT ?x ?lf WHERE {
-    #         ?x a skos:Concept;
-    #         skosxl:prefLabel ?l.
-    #         ?l skosxl:literalForm ?lf.
-    #     }
-    #     """, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#"),
-    #                      skosxl=Namespace("http://www.w3.org/2008/05/skos-xl#")))
+        alt_labels = graph.query("""SELECT ?x ?lf WHERE {
+            ?x a skos:Concept;
+            skosxl:altLabel ?l.
+            ?l skosxl:literalForm ?lf.
+        }
+        """, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#"),
+                         skosxl=Namespace("http://www.w3.org/2008/05/skos-xl#")))
 
-    #     for result in pref_labels:
-    #         string_entries.append((str(result[0]), str(result[1])))
+        for result in alt_labels:
+            string_entries.append((str(result[0]), str(result[1])))
+        dictionary_loader = StringDictionaryLoader(string_entries)
+        dictionary_loader.load()
 
-    #     alt_labels = graph.query("""SELECT ?x ?lf WHERE {
-    #         ?x a skos:Concept;
-    #         skosxl:altLabel ?l.
-    #         ?l skosxl:literalForm ?lf.
-    #     }
-    #     """, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#"),
-    #                      skosxl=Namespace("http://www.w3.org/2008/05/skos-xl#")))
+        self.concept_recognizer = IntersStemConceptRecognizer(dictionary_loader,
+                                                              "data/stopwordsfr.txt",
+                                                              "data/termination_termsfr.txt")
+        self.concept_recognizer.initialize()
 
-    #     for result in alt_labels:
-    #         string_entries.append((str(result[0]), str(result[1])))
-    #     dictionary_loader = StringDictionaryLoader(string_entries)
-    #     dictionary_loader.load()
-
-    #     self.concept_recognizer = IntersStemConceptRecognizer(dictionary_loader,
-    #                                                           "data/stopwordsfr.txt",
-    #                                                           "data/termination_termsfr.txt")
-    #     self.concept_recognizer.initialize()
-
-    # def find_with_agrovoc(self, keyword):
-    #     matching_annotations = self.concept_recognizer.recognize(keyword)
-    #     return_annotations = set()
-    #     for matching_annotation in matching_annotations:
-    #         delta = matching_annotation.end - matching_annotation.start
-    #         if len(keyword) == delta:
-    #             return_annotations.add((matching_annotation.concept_id, matching_annotation.matched_text))
-    #     return return_annotations
+    def find_with_agrovoc(self, keyword):
+        matching_annotations = self.concept_recognizer.recognize(keyword)
+        return_annotations = set()
+        for matching_annotation in matching_annotations:
+            delta = matching_annotation.end - matching_annotation.start
+            if len(keyword) == delta:
+                return_annotations.add((matching_annotation.concept_id, matching_annotation.matched_text))
+        return return_annotations
