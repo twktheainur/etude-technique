53e3392a0cda1ca92e642f0de3eb0ea39833f098
Andon Tchechmedjiev <twk.theainur@gmail.com>
2021-02-19 19:46:22 +0100

    Fix for annotations

--- agrovoc.py
+++ agrovoc.py
@@ -23,45 +23,44 @@ class Agrovoc:
         string_entries = []
 
         if skos_xl_labels:
-            query = """SELECT ?x ?lf WHERE {
+            query = f"""SELECT ?x ?lf WHERE {{
                 ?x a skos:Concept;
                 skosxl:prefLabel ?l.
                 ?l skosxl:literalForm ?lf.
                 FILTER(lang(?lf)='{lang}')
-            }
-            """.format(lang=lang)
+            }}
+            """
             pref_labels = self.graph.query(query, initNs={'skos': Namespace("http://www.w3.org/2004/02/skos/core#"),
                                                           'skosxl': Namespace("http://www.w3.org/2008/05/skos-xl#")})
         else:
-            query = """SELECT ?x ?lf WHERE {
+            query = f"""SELECT ?x ?lf WHERE {{
                  ?x a skos:Concept;
                  skos:prefLabel ?lf.
                  FILTER(lang(?lf)='{lang}')
-             }
-             """.format(lang=lang)
+             }}
+             """
             pref_labels = self.graph.query(query, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#")))
 
         for result in pref_labels:
             string_entries.append((str(result[0]), str(result[1])))
 
         if skos_xl_labels:
-            query = """SELECT ?x ?lf WHERE {
+            query = f"""SELECT ?x ?lf WHERE {{
                 ?x a skos:Concept;
                 skosxl:prefLabel ?l.
                 ?l skosxl:literalForm ?lf.
                 FILTER(lang(?lf)='{lang}')
-            }
-        """.format(lang=lang)
+            }}
+        """
             alt_labels = self.graph.query(query, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#"),
                                                              skosxl=Namespace("http://www.w3.org/2008/05/skos-xl#")))
         else:
-            query = """SELECT ?x ?lf WHERE {
+            query = f"""SELECT ?x ?lf WHERE {{
             ?x a skos:Concept;
             skos:altLabel ?lf.
-            FILTER(lang(?lf)='"""+lang+"""')
-
-        }
-        """.format(lang=lang)
+            FILTER(lang(?lf)='{lang}')
+        }}
+        """
             alt_labels = self.graph.query(query, initNs=dict(skos=Namespace("http://www.w3.org/2004/02/skos/core#")))
 
         for result in alt_labels:
@@ -89,3 +88,6 @@ class Agrovoc:
                 return_annotations.add((matching_annotation.concept_id, matching_annotation.matched_text,
                                         matching_annotation.start, matching_annotation.end))
         return return_annotations
+
+    def annotate_text(self, text):
+        return self.concept_recognizer.recognize(text)
